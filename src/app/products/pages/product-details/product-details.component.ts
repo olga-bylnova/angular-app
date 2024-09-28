b3fbea8dff327ece27bec9446ad6ccf07c7a4f7c
olga <olga.bylnova@yandex.by>
2024-09-28 03:10:26 +0300

    fix adding to cart on product details page

--- src/app/products/pages/product-details/product-details.component.ts
+++ src/app/products/pages/product-details/product-details.component.ts
@@ -9,7 +9,9 @@ import { CommonModule } from '@angular/common';
 import { Review } from '../../models/review';
 import { ProductService } from '../../services/product.service';
 import { CustomerReviewComponent } from '../../components/customer-review/customer-review.component';
-import { Observable, tap } from 'rxjs';
+import { EMPTY, Observable, tap } from 'rxjs';
+import { CartService } from '../../../cart/services/cart.service';
+import { CartItem } from '../../../cart/models/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -21,19 +23,27 @@ import { Observable, tap } from 'rxjs';
 export class ProductDetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   productService: ProductService = inject(ProductService);
-  product$: Observable<Product>;
-  reviews$: Observable<Review[]>;
+  cartService: CartService = inject(CartService);
+  product$: Observable<Product> = EMPTY;
+  reviews$: Observable<Review[]> = EMPTY;
+  cartItem: CartItem | undefined;
   isOutOfStock: boolean = false;
   faStar = faStar;
   faDollarSign = faDollarSign;
 
-  constructor() {
+  ngOnInit() {
     const productId = Number(this.route.snapshot.params['id']);
-    this.product$ = this.productService.getProductById(productId).pipe(
-      tap(product => {
-        this.isOutOfStock = product.stock === 0;
-      })
-    );
-    this.reviews$ = this.productService.getReviewsByProductId(productId);
+    if (!isNaN(productId)) {
+      this.product$ = this.productService.getProductById(productId).pipe(
+        tap(product => {
+          this.isOutOfStock = !product.stock;
+
+          this.cartService.getCartItemByProductId(product.id).subscribe(data => {
+            this.cartItem = data;
+          });
+        })
+      );
+      this.reviews$ = this.productService.getReviewsByProductId(productId);
+    }
   }
 }
